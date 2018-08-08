d0f10bc1a5d452e144ee28fb179c0fe9917cdc5c
Maxim Sukharev <maxim@sourced.tech>
2018-08-09 01:23:43 +0200

    update driver versions

--- start-bblfshd.sh
+++ start-bblfshd.sh
@@ -2,8 +2,8 @@ bblfshd &
 sleep 1
 echo "installing drivers"
 sleep 1
-bblfshctl driver install python docker://bblfsh/python-driver:v2.1.2
-bblfshctl driver install java docker://bblfsh/java-driver:v2.0.2
-bblfshctl driver install javascript docker://bblfsh/javascript-driver:v2.0.2
+bblfshctl driver install python docker://bblfsh/python-driver:v2.2.2
+bblfshctl driver install java docker://bblfsh/java-driver:v2.1.0
+bblfshctl driver install javascript docker://bblfsh/javascript-driver:v2.1.1
 echo "all drivers installed"
 tail -f /dev/null
